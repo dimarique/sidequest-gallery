f52e9ff560e0e043670fead9e429ca8abcc14e7b
dimarique <pochtagreen@gmail.com>
2025-07-21 09:53:02 +0200

    move PlayStopButton files to components, fix some styling and links

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 
 import { useState } from "react";
 
-
 import img0 from "./data/image_0.jpg";
 import img1 from "./data/image_1.jpg";
 import img2 from "./data/image_2.jpg";
@@ -16,13 +15,12 @@ import img9 from "./data/image_9.jpg";
 import GalleryContent from "./components/GalleryContent/GalleryContent";
 import ButtonNext from "./components/ButtonNext/ButtonNext";
 import ButtonPrev from "./components/ButtonPrev/ButtonPrev";
-
+import PlayStopButton from "./components/PlayStopButton/PlayStopButton";
 import ButtonWrapper from "./components/ButtonWrapper/ButtonWrapper";
 import ImagesRibbon from "./components/ImagesRibbon/ImagesRibbon";
 import LoginForm from "./components/LoginForm/LoginForm";
 
 function App() {
-
   const images = [img0, img1, img2, img3, img4, img5, img6, img7, img8, img9];
 
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -35,22 +33,24 @@ function App() {
         setCurrentIndex={setCurrentIndex}
         images={images}
       />
-      <ButtonNext
-        currentIndex={currentIndex}
-        setCurrentIndex={setCurrentIndex}
-        images={images}
-      />
-      <PlayStopButton
-        currentIndex={currentIndex}
-        setCurrentIndex={setCurrentIndex}
-        images={images}
-      />
-
-      <ImagesRibbon
-        images={images}
-        setCurrentIndex={setCurrentIndex}
-         />
-
+      <ImagesRibbon images={images} setCurrentIndex={setCurrentIndex} />
+      <ButtonWrapper>
+        <ButtonPrev
+          currentIndex={currentIndex}
+          setCurrentIndex={setCurrentIndex}
+          images={images}
+        />
+        <PlayStopButton
+          currentIndex={currentIndex}
+          setCurrentIndex={setCurrentIndex}
+          images={images}
+        />
+        <ButtonNext
+          currentIndex={currentIndex}
+          setCurrentIndex={setCurrentIndex}
+          images={images}
+        />
+      </ButtonWrapper>
     </>
   );
 }

8b198a8c4932246a1c3c17c067fe4dafbe940a89
dimarique <pochtagreen@gmail.com>
2025-07-27 11:40:28 +0200

    unify code formatting

--- src/components/ButtonPrev/ButtonPrev.jsx
+++ src/components/ButtonPrev/ButtonPrev.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import styles from '../ButtonStyles/ButtonStyles.module.css'
+import React from "react";
+import styles from "../ButtonStyles/ButtonStyles.module.css";
 
 const ButtonPrev = ({ currentIndex, setCurrentIndex, images }) => {
+  const handlePrev = () => {
+    currentIndex === 0
+      ? setCurrentIndex(images.length - 1)
+      : setCurrentIndex(currentIndex - 1);
+  };
 
-    const handlePrev = () => {
-        currentIndex === 0 ? setCurrentIndex(images.length - 1) : setCurrentIndex(currentIndex - 1)
-    }
+  return (
+    <>
+      <button className={styles.buttonPrev} onClick={handlePrev}>
+        Prev
+      </button>
+    </>
+  );
+};
 
-    return (
-        <>
-            <button className={styles.buttonPrev} onClick={handlePrev}>Prev</button>
-        </>
-    )
-}
-
-export default ButtonPrev
+export default ButtonPrev;
